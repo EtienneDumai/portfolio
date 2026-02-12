3caa886de49930eecaff88b8a374bcef22df163d
EtienneDumai <etienne.dumai@gmail.com>
2026-02-12 13:21:36 +0100

    page des projets avec les cards et les boucles

--- mon-portfolio/src/app/components/project/project.ts
+++ mon-portfolio/src/app/components/project/project.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProjectService } from '../../services/ProjectService/project-service';
+import { Project } from '../../data/data';
+import { ProjectCard } from '../project-card/project-card';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  standalone: true,
+  imports: [ProjectCard],
   templateUrl: './project.html',
   styleUrl: './project.css',
 })
-export class Project {
-
+export class ProjectComponent implements OnInit {
+  private readonly projectService = inject(ProjectService);
+  protected listProjet !: Project[];
+  ngOnInit(): void {
+    this.projectService.getAllProjects().subscribe({
+      next: (data) => {
+        this.listProjet = data;
+      },
+      error: (err) => {
+        console.error(`Une erreur est survenue lors de la recupération des projets : ${err}`);
+      }
+    })
+  }
 }
